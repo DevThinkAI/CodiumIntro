53d94d38964da332d04651f2d65ecf488b70b443
Sam Keen <sam.sjk@gmail.com>
2023-07-17 12:38:50 -0700

    init commit prior to using Codium

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, request, render_template
+import requests
+import json
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        postal_code = request.form['postal_code']
+        weather = get_weather(postal_code)
+        return render_template('index.html', weather=weather)
+    else:
+        return render_template('index.html')
+
+def get_weather(postal_code):
+    api_key = "35e3797f7462621c940662fb3f6ade7a"
+    base_url = "http://api.openweathermap.org/data/2.5/weather"
+    complete_url = base_url + "?appid=" + api_key + "&zip=" + postal_code
+    response = requests.get(complete_url)
+    return response.json()
+
+if __name__ == "__main__":
+    app.run(debug=True)
